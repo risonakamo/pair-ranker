ee995726c60dc6be2fdd1e34c09574d9c8c3488d
risonakamo <hmykanae@gmail.com>
2020-12-13 16:36:04 -0500

    pair choice designing

--- components/pair-rank/components/pair-choice/pair-choice.tsx
+++ components/pair-rank/components/pair-choice/pair-choice.tsx
@@ -5,10 +5,16 @@ import "./pair-choice.less";
 export default function PairChoice():JSX.Element
 {
   return <div className="pair-choice">
-    <h1 className="choice left">choice1</h1>
+    <h1 className="choice left">
+      choice1
+      <img src="assets/img/chevron-left.svg" className="choice-arrow"/>
+    </h1>
     <div className="mid-split">
 
     </div>
-    <h1 className="choice right">choice2</h1>
+    <h1 className="choice right">
+      <img src="assets/img/chevron-right.svg" className="choice-arrow"/>
+      choice2
+    </h1>
   </div>;
 }

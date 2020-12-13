888522dfbdf76dae87bcee67bf44ba777b320839
risonakamo <hmykanae@gmail.com>
2020-12-13 17:00:48 -0500

    pair choice designing trying react svg

--- components/pair-rank/components/pair-choice/pair-choice.tsx
+++ components/pair-rank/components/pair-choice/pair-choice.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import {ReactSVG} from "react-svg";
 
 import "./pair-choice.less";
 
@@ -7,13 +8,13 @@ export default function PairChoice():JSX.Element
   return <div className="pair-choice">
     <h1 className="choice left">
       choice1
-      <img src="assets/img/chevron-left.svg" className="choice-arrow"/>
+      <ReactSVG src="assets/img/chevron-left.svg" className="choice-arrow"/>
     </h1>
     <div className="mid-split">
 
     </div>
     <h1 className="choice right">
-      <img src="assets/img/chevron-right.svg" className="choice-arrow"/>
+      <ReactSVG src="assets/img/chevron-right.svg" className="choice-arrow"/>
       choice2
     </h1>
   </div>;
